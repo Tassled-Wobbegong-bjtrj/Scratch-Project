dd6ad66b1c2ba3f2ec71537ff49c14a25f147153
Tingg123 <lidanting123@gmail.com>
2024-04-28 17:34:41 -0400

    1st front-end

---
+++ client/src/Components/DinnerPlans.js
@@ -0,0 +1,20 @@
+// ❤️❤️ ❤️❤️ ❤️❤️
+import React, { useState } from 'react'
+
+const DinnerPlans = ({ onAnswer }) => {
+  const [craving, setCraving] = useState('')
+
+  const handleNext = () => {
+    onAnswer('dinnerPlans', craving)
+  }
+
+  return (
+    <div>
+      <p>Cool, what about dinner plans, is there anything you're craving?</p>
+      <input className="dinner-input" type="text" value={craving} onChange={(e) => setCraving(e.target.value)} />
+      <button onClick={handleNext}>Next</button>
+    </div>
+  )
+}
+
+export default DinnerPlans
